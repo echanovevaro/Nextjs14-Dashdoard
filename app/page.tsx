5ca7d2a200b62d5f1572488f918b95d055a3c7ab
echanovevaro <30557018+echanovevaro@users.noreply.github.com>
2023-11-17 16:47:46 +0100

    metadata and finished
    Signed-off-by: echanovevaro <30557018+echanovevaro@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ import { lusitana } from "@/app/ui/fonts"
 
 import Link from "next/link"
 import Image from "next/image"
+import { Metadata } from "next"
+export const metadata: Metadata = {
+  title: "Home",
+}
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
